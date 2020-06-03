6777c00dd2134cc839028d36eec2f314274e6d03
kush1902 <lakhmanikushagra@gmail.com>
2020-06-03 19:58:24 +0530

    Scanner code

---
+++ android/app/src/main/kotlin/com/example/qr1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.qr1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+import 'qr_scanner.dart';
+void main() => runApp( new MaterialApp(
+
+  home: HomePage(),
+),
+);
+
